8eac4c8f9c4a6efad62ea5e3444e64853860f7bf
Anusha Akhter ❤️ <akhteranusha38@gmail.com>
2025-12-11 10:32:15 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+function checkvowels(){
+    let inputBox = document.getElementById('inputBox').value;
+    let result = document.getElementById('result');
+    let error = document.getElementById('error');
+
+    result.textContent='';
+    error.textContent='';
+
+    if(!inputBox){
+        error.textContent = 'Please enter some text';
+        error.style.color = 'red';
+        return;
+    }
+
+    let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    let count = 0;
+
+    for (let i=0; i < inputBox.length; i++) {
+        if (vowels.includes(inputBox[i])) {
+          count++ ;
+        }
+    }
+    result.textContent = `Number of vowels:  ${count}`;
+}
